238d2b9859809a465902428e3ff6ef75f5602d58
GoncaloF-ATEC <Goncalo.Feliciano.0001150@edu.atec.pt>
2023-07-29 18:46:05 +0100

    Desafio done

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ extrato.
 
 
 '''
-
+import os
 
 menu = """
     Banco 
@@ -23,29 +23,82 @@ menu = """
     [Q]uit - Sair
 """
 
-print(menu)
+extrato = """
+Extrato da conta\n
+"""
 
-
-saldo_inicial = 1000
+saldo = 1000
 total_saques = 0
 
 SAQUE_MAX = 500
-MAX_SAQUES = 3
+MAX_SAQUES = 1
 
+extrato += f"saldo inicial -> {saldo: .2f}\n"
 
 while True:
 
-    op = input("\tSelecione a opção desejada: ")
+    print(menu)
+
+    op = input("Selecione a opção desejada: ")
 
     match op.lower():
 
         case 's':
-            print("saque")
+            os.system("cls | clear")
+            valor = input("\tValor a sacar? \n\t")
+
+            if int(valor) > SAQUE_MAX:
+                 print(f"o saque maximo é de R$ {SAQUE_MAX}")
+
+            elif saldo < int(valor):
+                print("Valor insuficiente")
+
+            elif total_saques >= MAX_SAQUES:
+                print("Ja faz todo os sques premitos num dia")
+
+            else:
+                total_saques += 1
+                saldo -= int(valor)
+
+                print("\tSaque feito")
+                print(f"\tsaldo atual R$ {saldo:.2f}")
+
+
+                extrato += f"saque R$ {int(valor):.2f}\n"
+                extrato += f"saldo atual R$ {saldo:.2f}\n"
+
+
+
+            input("presione uma tecla para continua")
         case 'd':
-            print("depoisito")
+            valor = input("\tValor a depositar? \n\t")
+
+            if valor.isnumeric():
+                saldo += int(valor)
+
+                extrato += f"deposito R$ {int(valor):.2f}\n"
+                extrato += f"saldo atual R$ {saldo:.2f}\n"
+
+                print("\tvalor depositado")
+                print(f"\tsaldo atual R$ {saldo:.2f}")
+
+
+            else:
+                print("o valor deve ser numerico")
+
+
         case 'e':
-            print("Extrato")
+            os.system("cls | clear")
+            print("inicio do estrato".center(25, "-"))
+            print(extrato)
+
+            print("fim do estrato".center(25, "-"))
+
+            input("presione uma tecla para continua")
         case 'q':
             exit(0)
         case _:
-            print("\topeção invalida tente de novo")
+            print("opeção invalida tente de novo")
+            input("presione uma tecla para continua")
+
+    os.system("cls | clear")
